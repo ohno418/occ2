a3940d6e3b1dc6ba4dc4e5964b82cfa76259b97b
yutarofire <yutaro.ono.418@gmail.com>
2020-12-08 02:46:12 +0900

    Tokenize the input string

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <string.h>
+
+typedef enum {
+  TK_PUNCT, // Puctuators
+  TK_NUM,   // Numeric literals
+  TK_EOF,   // End-of-file markers
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;   // If token is TK_NUM, its value
+  char *loc; // Token location
+  int len;   // Token length
+};
 
 // Report an error and exit.
 static void error(char *fmt, ...) {
@@ -11,40 +29,91 @@ static void error(char *fmt, ...) {
   exit(1);
 }
 
+static Token *new_token(TokenKind kind, char *loc, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->loc = loc;
+  tok->len = len;
+  return tok;
+}
+
+static Token *tokenize(char *p) {
+  Token head;
+  Token *cur = &head;
+
+  while (*p) {
+    // Skip whitespace characters.
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // Numeric literal
+    if (isdigit(*p)) {
+      char *q = p;
+      int num = strtol(p, &p, 10);
+      cur = cur->next = new_token(TK_NUM, p, p - q);
+      cur->val = num;
+      cur->len = p - q;
+      continue;
+    }
+
+    // Puctuator
+    if (*p == '+' || *p == '-') {
+      cur = cur->next = new_token(TK_PUNCT, p, 1);
+      p++;
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  cur = cur->next = new_token(TK_EOF, p, 0);
+  return head.next;
+}
+
+// Surely get an number from the given token.
+static int get_number(Token *tok) {
+  if (tok->kind != TK_NUM)
+    error("expected a number");
+  return tok->val;
+}
+
+static bool equal(Token *tok, char *op) {
+  return tok->len == strlen(op) && memcmp(tok->loc, op, tok->len) == 0;
+}
+
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
-  char *p = argv[1];
+  Token *tok = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == ' ') {
-      p++;
+  // The first token must be a number.
+  printf("  mov rax, %d\n", get_number(tok));
+  tok = tok->next;
+
+  while (tok->kind != TK_EOF) {
+    if (equal(tok, "+")) {
+      printf("  add rax, %d\n", get_number(tok->next));
+      tok = tok->next->next;
       continue;
     }
 
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+    if (equal(tok, "-")) {
+      printf("  sub rax, %d\n", get_number(tok->next));
+      tok = tok->next->next;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    error("unexpected character '%c'", *p);
+    error("invalid token");
   }
 
   printf("  ret\n");
-
   exit(0);
 }
