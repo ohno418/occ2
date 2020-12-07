a3f484ed4a603fb316b6496683dcdcaf38fe8c46
yutarofire <yutaro.ono.418@gmail.com>
2020-12-08 00:46:09 +0900

    Compile an integer to an assembly file that exits with the given number

--- test.sh
+++ test.sh
@@ -1,5 +1,22 @@
-./occ > tmp.s
-gcc -static -o tmp tmp.s
-./tmp
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
 
-echo $?
+  ./occ "$input" > tmp.s
+  gcc -static -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
