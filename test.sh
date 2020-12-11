635ed0631b8d4e3297e7ee244dfd0ac71b5386c6
yutarofire <yutaro.ono.418@gmail.com>
2020-12-11 18:46:06 +0900

    Change test code details

--- test.sh
+++ test.sh
@@ -18,25 +18,30 @@ assert() {
 
 assert 0 0
 assert 42 42
+
 assert 25 '2+23'
 assert 11 '23-12'
 assert 57 '42+3+12'
 assert 33 '42+3-12'
 assert 33 '42  + 3- 12'
+
 assert 35 '5*7'
 assert 38 '3+5*7'
 assert 4 '8/2'
 assert 7 '3+8/2'
 assert 56 '(3+5)*7'
 assert 5 '(3+8)/2'
+
 assert 10 '-10+20'
 assert 10 '--10'
 assert 10 '-- +10'
 assert 10 '20+(-10)'
+
 assert 1 '3==3'
 assert 0 '3==4'
 assert 0 '3!=3'
 assert 1 '3!=4'
+
 assert 1 '24<42'
 assert 0 '42<24'
 assert 0 '24<24'
