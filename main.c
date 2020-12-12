228b2e58f89b60c7cc26b1a31a5c4aaeebadfe55
yutarofire <yutaro.ono.418@gmail.com>
2020-12-12 19:32:38 +0900

    Move debug code to main.c from tokenize.c

--- main.c
+++ main.c
@@ -10,3 +10,19 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+//
+// debug tools
+//
+
+// printf("===== token debug =====\n");
+// int i = 0;
+// for (Token *t = tok; t->kind != TK_EOF; t = t->next) {
+//   printf("token %d\n", i);
+//   printf("  kind: %d\n", t->kind);
+//   printf("  val: %d\n", t->val);
+//   printf("  loc: %s\n", t->loc);
+//   printf("  len: %d\n", t->len);
+//   i++;
+// }
+// printf("===== debug end =====\n");
--- tokenize.c
+++ tokenize.c
@@ -101,15 +101,3 @@ Token *tokenize(char *p) {
   cur = cur->next = new_token(TK_EOF, p, 0);
   return head.next;
 }
-
-// printf("===== token debug =====\n");
-// int i = 0;
-// for (Token *t = tok; t->kind != TK_EOF; t = t->next) {
-//   printf("token %d\n", i);
-//   printf("  kind: %d\n", t->kind);
-//   printf("  val: %d\n", t->val);
-//   printf("  loc: %s\n", t->loc);
-//   printf("  len: %d\n", t->len);
-//   i++;
-// }
-// printf("===== debug end =====\n");
